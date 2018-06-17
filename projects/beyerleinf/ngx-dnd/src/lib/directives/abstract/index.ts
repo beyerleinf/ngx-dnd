a4861f471325eec427cfe0afe1b877af870b018e
Fabian Beyerlein <dev@dazeeeh.com>
2018-06-17 20:56:48 +0200

    chore(refactoring): Refactored draggable and droppable directives

---
+++ projects/beyerleinf/ngx-dnd/src/lib/directives/abstract/index.ts
@@ -0,0 +1,2 @@
+export {AbstractHandleDirective} from './abstract-handle.directive';
+export {AbstractDirective} from './abstract.directive';
--- projects/beyerleinf/ngx-dnd/src/lib/directives/draggable-handle.directive.ts
+++ projects/beyerleinf/ngx-dnd/src/lib/directives/draggable-handle.directive.ts
@@ -3,15 +3,15 @@ import {ChangeDetectorRef, Directive, ElementRef} from '@angular/core';
 import {DragDropConfig} from '../config';
 import {DragDropService} from '../service';
 
-import {AbstractHandleDirective} from './abstract-handle.directive';
+import {AbstractHandleDirective} from './abstract';
 import {DraggableDirective} from './draggable.directive';
 
 /* tslint:disable directive-selector */
-@Directive({selector: '[dnd-draggable-handle]'})
+@Directive({selector: '[dnd-draggable-handle'})
 export class DraggableHandleDirective extends AbstractHandleDirective {
   constructor(
-      elemRef: ElementRef, dragDropService: DragDropService, config: DragDropConfig, _Component: DraggableDirective,
-      cdr: ChangeDetectorRef) {
-    super(elemRef, dragDropService, config, _Component, cdr);
+      elementReference: ElementRef, dragDropService: DragDropService, config: DragDropConfig,
+      directive: DraggableDirective, cdr: ChangeDetectorRef) {
+    super(elementReference, dragDropService, config, directive, cdr);
   }
 }
