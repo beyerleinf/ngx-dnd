2ff4be82c9b33da53b356d5e73e7846f0a09eb8a
Fabian Beyerlein <mail@fabian-b.de>
2019-02-06 20:09:11 +0100

    chore(): Removed commented-out code

--- projects/beyerleinf/ngx-dnd/src/lib/dnd.module.ts
+++ projects/beyerleinf/ngx-dnd/src/lib/dnd.module.ts
@@ -1,27 +1,9 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 
 import {DragDropConfig} from './config';
-// import {DragDropConfig} from './dnd.config';
-// import {DragDropService, dragDropServiceFactory, DragDropSortableService, dragDropSortableServiceFactory} from
-// './dnd.service'; import {DraggableComponent, DraggableHandleComponent} from './draggable.component'; import
-// {DroppableComponent} from './droppable.component'; import {SortableComponent, SortableContainer,
-// SortableHandleComponent} from './sortable.component';
-
 import {DraggableDirective, DraggableHandleDirective, DroppableDirective} from './directives';
 import {SortableContainerDirective, SortableDirective, SortableHandleDirective} from './directives';
 import {DragDropService, dragDropServiceFactory, DragDropSortableService, dragDropSortableServiceFactory} from './service';
-
-// export * from './abstract.component';
-// export * from './dnd.config';
-// export * from './dnd.service';
-// export * from './draggable.component';
-// export * from './droppable.component';
-// export * from './sortable.component';
-
-export * from './config';
-export * from './directives';
-export * from './models';
-export * from './service';
 
 export let providers = [
   DragDropConfig, {provide: DragDropService, useFactory: dragDropServiceFactory},
@@ -30,12 +12,20 @@ export let providers = [
 
 @NgModule({
   declarations: [
-    DraggableDirective, DraggableHandleDirective, DroppableDirective, SortableContainerDirective, SortableDirective,
-    SortableHandleDirective
+    DraggableDirective,
+    DraggableHandleDirective,
+    DroppableDirective,
+    SortableContainerDirective,
+    SortableDirective,
+    SortableHandleDirective,
   ],
   exports: [
-    DraggableDirective, DraggableHandleDirective, DroppableDirective, SortableContainerDirective, SortableDirective,
-    SortableHandleDirective
+    DraggableDirective,
+    DraggableHandleDirective,
+    DroppableDirective,
+    SortableContainerDirective,
+    SortableDirective,
+    SortableHandleDirective,
   ],
 
 })
