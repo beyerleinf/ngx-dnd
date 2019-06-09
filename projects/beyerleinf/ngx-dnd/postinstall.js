3cde0ee96bf3e365b963992269215fb5caeb64a9
beyerleinf <mail@fabian-b.de>
2019-06-09 14:45:06 +0200

    chore(): Added postinstall notice

---
+++ projects/beyerleinf/ngx-dnd/postinstall.js
@@ -0,0 +1,6 @@
+console.log(
+  '\n\n\u001b[96mWe recommend switching to native drag and drop using Angular CDK (\u001B[94mhttps://material.angular.io/cdk/drag-drop/overview\u001B[96m)\u001b[0m'
+);
+console.log(
+  '\u001b[96mThis package will probably not be updated again for Angular 9 which will be coming in Fall 2019.\u001b[0m\n\n'
+);
